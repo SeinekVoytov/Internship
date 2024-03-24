41fa9fee8dbebdae3c3a749f3f66cc0c822faac8
SeinekVoytov <voytov1704@gmail.com>
2024-03-24 12:51:30 +0300

    Refactor: remove unnecessary dependencies from SpringConfig, switch junit version to the later version, add maven compiler plugin version

--- Task2/src/main/java/org/example/config/SpringConfig.java
+++ Task2/src/main/java/org/example/config/SpringConfig.java
@@ -1,14 +1,12 @@
 package org.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
